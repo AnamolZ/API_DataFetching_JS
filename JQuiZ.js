d72fc49f2c305f95bf22ec02d77b5f4bb6f48a1b
Anamol <anmoldkl971@gmail.com>
2023-06-14 13:42:58 +0545

    PDF Report Function Added

--- JQuiZ.js
+++ JQuiZ.js
@@ -1,6 +1,10 @@
 let correctCount = 0;
 let questionCount = 0;
 let sequenceAnswering = [];
+let userAnswers = [];
+let correctAnswers = [];
+let questionList = [];
+let buttonList = [];
 
     // Use of Async/Await to Retrieve Data from API.
 async function dataRetrieval(url) {
@@ -19,7 +23,16 @@ function createButton(text, value, name) {
   return button;
 }
 
+  // Function to shuffle an array using Fisher-Yates algorithm. -- Source - https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleButton(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 async function renderQuestion() {
+  buttonList = []; // Empty the buttonList array.
   const results = await dataRetrieval("https://opentdb.com/api.php?amount=1&category=18&type=multiple"); // Use of Async/Await.
   const { correct_answer, incorrect_answers, category, difficulty, question } = results[0]; // Use of Destructuring.
 
@@ -28,18 +41,27 @@ async function renderQuestion() {
   document.getElementById("difficulty").textContent = "Difficulty: " + difficulty;
   document.getElementById("question").textContent = question;
 
+  questionList.push(question);
+    // Use of Array.push() to Add Correct Answers to Array.
+  correctAnswers.push(correct_answer);
+
   const optionsContainer = document.getElementById("options");
     // Use of DOM Manipulation to Clear Previous Answers.
   optionsContainer.innerHTML = "";
 
-        // Use of DOM Manipulation to Create Correct Answer Button
   const correctButton = createButton(correct_answer, correct_answer, "correct");
-        // Use of DOM Manipulation to Append Correct Answer Button to Options Container.
-  optionsContainer.appendChild(correctButton);
+  buttonList.push(correctButton);
 
-        // Use of Array.forEach() to Iterate Through Incorrect Answers.
   incorrect_answers.forEach(incorrectAnswer => {
     const button = createButton(incorrectAnswer, incorrectAnswer, "incorrect");
+    buttonList.push(button);
+  });
+
+    // Shuffle the array of buttons.
+  shuffleButton(buttonList);
+
+    // Append the shuffled buttons to the optionsContainer.
+    buttonList.forEach(button => {
     optionsContainer.appendChild(button);
   });
 }
@@ -49,6 +71,8 @@ function buttonHandler(event) {
     //Value and Name will be assigned after event.target object is created with corresponding properties value and name.
   const value = event.target.value; // Object event.target represents the button element that triggered the event.
   const name = event.target.name; // Object event.target is property of buttonHandler. 
+    // Use of Array.push() to Add User Answers to Array.
+  userAnswers.push(value);
 
   if (name === "correct") {
     event.target.classList.add("correct"); // Adds the CSS class "correct" to the button element.
@@ -59,21 +83,32 @@ function buttonHandler(event) {
     sequenceAnswering.push(0);
   }
 
-  console.log(value);
-  console.log("Correct Answers: " + correctCount);
-
   questionCount++;
 
   if (questionCount === 10) {
+    buttonList = []; // Empty the buttonList array.
     showScore();
   } else {
+    buttonList = []; // Empty the buttonList array.
     renderQuestion();
   }
 }
 
 function downloadPDF() {
-  let pdf = new jsPDF("landscape", "mm", "a4");
-  pdf.text("Hello World", 10, 10);
+  // Use of jsPDF Library to Create PDF.
+  let pdf = new jsPDF("portrait", "mm", [400, 700]);
+  let yPos = 10;
+  questionList.forEach((question, index) => {
+    // Use of Array.forEach() to Iterate Through User Answers.
+    const userAnswer = userAnswers[index];
+    const correctAnswer = correctAnswers[index];
+
+    pdf.text(`Question: ${question}`, 10, yPos);
+    pdf.text(`User's Answer: ${userAnswer}`, 10, yPos + 10);
+    pdf.text(`Correct Answer: ${correctAnswer}`, 10, yPos + 20);
+    yPos += 40;
+  });
+
   pdf.save('report.pdf');
 }
 
@@ -103,7 +138,7 @@ function showScore() {  // Use of DOM Manipulation to Display Final Score.
   mainContent.appendChild(button);
 
   const data = {
-
+      // Data to be Represented on Chart.
     labels: ['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10'],
     datasets: [{
         label: "QU'Z Report",
@@ -113,6 +148,7 @@ function showScore() {  // Use of DOM Manipulation to Display Final Score.
     }]
     };
 
+      // Configuration For Chart.
     const config = {
     type: 'line',
     data: data,
